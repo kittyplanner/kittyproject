01f11d7ddcc961c842c8a18ed3963b1e6d97c384
haesukiim <haesukim03@gmail.com>
2024-11-14 15:25:44 +0900

    테스트 주석 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 //fkdjnfksdjnfjksnks
+  //두번째 테스트 주석
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
